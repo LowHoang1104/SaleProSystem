11de8286f9ea3ef36558e801e257c269f69af141
LowHoang1104 <long110604@gmail.com>
2025-07-20 22:23:35 +0700

    Update AuthFilter.java

--- src/java/Filter/AuthFilter.java
+++ src/java/Filter/AuthFilter.java
@@ -1,15 +1,9 @@
-package Filter;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package filter;
 
-
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -20,228 +14,89 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import salepro.dao.PermissionDAO;
+import salepro.dao.UserDAO;
+import salepro.models.Users;
 
 /**
  *
- * @author ADMIN
+ * @author Thinhnt
  */
-@WebFilter(filterName = "AuthFilter", urlPatterns = {"/view/jsp/employees/*","/view/jsp/admin/*","/view/jsp/employee/*","/HomepageController","/ListUserServlet","/CashierServlet","/cashbookController","/ListCustomerServlet","/ListUserPermissionServlet","/ListWorkScheduleServlet"})
-
+@WebFilter("/*")
 public class AuthFilter implements Filter {
-    
-    private static final boolean debug = true;
 
-    // The filter configuration object we are associated with.  If
-    // this value is null, this filter instance is not currently
-    // configured. 
-    private FilterConfig filterConfig = null;
-    
-    public AuthFilter() {
-    }    
-    
-    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
-            throws IOException, ServletException {
-        if (debug) {
-            log("AuthFilter:DoBeforeProcessing");
-        }
+    //Những uri mà chỉ admin mới được truy cập
+    private static final Set<String> adminOnlyUris = Set.of("/ListUserServlet", "/ListUserPermissionServlet", "/ListShiftServlet");
+    private static final Set<String> employeeManagement = Set.of("/ListWorkScheduleServlet", "/ListAttendanceServlet");
 
-        // Write code here to process the request and/or response before
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log items on the request object,
-        // such as the parameters.
-        /*
-	for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
-	    String name = (String)en.nextElement();
-	    String values[] = request.getParameterValues(name);
-	    int n = values.length;
-	    StringBuffer buf = new StringBuffer();
-	    buf.append(name);
-	    buf.append("=");
-	    for(int i=0; i < n; i++) {
-	        buf.append(values[i]);
-	        if (i < n-1)
-	            buf.append(",");
-	    }
-	    log(buf.toString());
-	}
-         */
-    }    
-    
-    private void doAfterProcessing(ServletRequest request, ServletResponse response)
-            throws IOException, ServletException {
-        if (debug) {
-            log("AuthFilter:DoAfterProcessing");
-        }
-
-        // Write code here to process the request and/or response after
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log the attributes on the
-        // request object after the request has been processed. 
-        /*
-	for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
-	    String name = (String)en.nextElement();
-	    Object value = request.getAttribute(name);
-	    log("attribute: " + name + "=" + value.toString());
-
-	}
-         */
-        // For example, a filter might append something to the response.
-        /*
-	PrintWriter respOut = new PrintWriter(response.getWriter());
-	respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
-         */
+    //Kiểm tra có phải file tĩnh không 
+    private boolean isStaticResource(String uri) {
+        return uri.endsWith("Homepage.jsp") || uri.endsWith(".css") || uri.endsWith(".js") || uri.endsWith(".png") || uri.endsWith(".jpg") || uri.endsWith(".jpeg") || uri.endsWith(".gif") || uri.endsWith(".svg") || uri.endsWith(".woff") || uri.endsWith(".ttf") || uri.endsWith("eot");
     }
 
-    /**
-     *
-     * @param request The servlet request we are processing
-     * @param response The servlet response we are creating
-     * @param chain The filter chain we are processing
-     *
-     * @exception IOException if an input/output error occurs
-     * @exception ServletException if a servlet error occurs
-     */
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain)
-            throws IOException, ServletException {
-        
-        if (debug) {
-            log("AuthFilter:doFilter()");
-        }
-        
-        doBeforeProcessing(request, response);
-        
-         doBeforeProcessing(request, response);
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpRespond = (HttpServletResponse) response;
-        String url = httpRequest.getServletPath();
-        HttpSession session = httpRequest.getSession(); // tránh tạo session mới nếu chưa có
-
-        boolean loggedIn = session != null && session.getAttribute("user") != null;
-        if(session.getAttribute("user")==null){
-           httpRespond.sendRedirect("/Mg2/view/jsp/Homepage.jsp");
-        }
-        
-        Throwable problem = null;
-        try {
-            chain.doFilter(request, response);
-        } catch (Throwable t) {
-            // If an exception is thrown somewhere down the filter chain,
-            // we still want to execute our after processing, and then
-            // rethrow the problem after that.
-            problem = t;
-            t.printStackTrace();
-        }
-        
-        doAfterProcessing(request, response);
-
-        // If there was a problem, we want to rethrow it if it is
-        // a known type, otherwise log it.
-        if (problem != null) {
-            if (problem instanceof ServletException) {
-                throw (ServletException) problem;
-            }
-            if (problem instanceof IOException) {
-                throw (IOException) problem;
-            }
-            sendProcessingError(problem, response);
-        }
-    }
-
-    /**
-     * Return the filter configuration object for this filter.
-     */
-    public FilterConfig getFilterConfig() {
-        return (this.filterConfig);
-    }
-
-    /**
-     * Set the filter configuration object for this filter.
-     *
-     * @param filterConfig The filter configuration object
-     */
-    public void setFilterConfig(FilterConfig filterConfig) {
-        this.filterConfig = filterConfig;
-    }
-
-    /**
-     * Destroy method for this filter
-     */
-    public void destroy() {        
-    }
-
-    /**
-     * Init method for this filter
-     */
-    public void init(FilterConfig filterConfig) {        
-        this.filterConfig = filterConfig;
-        if (filterConfig != null) {
-            if (debug) {                
-                log("AuthFilter:Initializing filter");
-            }
-        }
-    }
-
-    /**
-     * Return a String representation of this object.
-     */
     @Override
-    public String toString() {
-        if (filterConfig == null) {
-            return ("AuthFilter()");
-        }
-        StringBuffer sb = new StringBuffer("AuthFilter(");
-        sb.append(filterConfig);
-        sb.append(")");
-        return (sb.toString());
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
-    
-    private void sendProcessingError(Throwable t, ServletResponse response) {
-        String stackTrace = getStackTrace(t);        
-        
-        if (stackTrace != null && !stackTrace.equals("")) {
-            try {
-                response.setContentType("text/html");
-                PrintStream ps = new PrintStream(response.getOutputStream());
-                PrintWriter pw = new PrintWriter(ps);                
-                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
 
-                // PENDING! Localize this for next official release
-                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
-                pw.print(stackTrace);                
-                pw.print("</pre></body>\n</html>"); //NOI18N
-                pw.close();
-                ps.close();
-                response.getOutputStream().close();
-            } catch (Exception ex) {
-            }
-        } else {
-            try {
-                PrintStream ps = new PrintStream(response.getOutputStream());
-                t.printStackTrace(ps);
-                ps.close();
-                response.getOutputStream().close();
-            } catch (Exception ex) {
-            }
-        }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
+        //Các DAO 
+        PermissionDAO permisionDao = new PermissionDAO();
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession();
+//        session.setAttribute("user", new UserDAO().getUserById(1));
+
+//        String uri = req.getRequestURI();
+//        String contextPath = req.getContextPath();
+//        String path = uri.substring(contextPath.length());
+//
+//        //Bỏ qua tài nguyên tĩnh 
+//        if (isStaticResource(uri)) {
+//            fc.doFilter(request, response);
+//            return;
+//        }
+//
+//        //Các trang không cần login
+//        if (path.contains("Login")) {
+//            fc.doFilter(request, response);
+//            return;
+//        }
+//
+//        //Kiểm tra đăng nhập
+////        HttpSession session = req.getSession(false);
+//        Users user = (session != null) ? (Users) session.getAttribute("user") : null;
+//        boolean isLoggedIn = (user != null);
+//        if (!isLoggedIn) {
+//            res.sendRedirect(contextPath + "/view/jsp/Login.jsp");
+//            return;
+//        }
+//
+//        //Phân quyền 
+//        int role = isLoggedIn ? user.getRoleId() : 0;
+//        if (role != 1) {
+//            if (adminOnlyUris.contains(path)) {
+//                res.sendRedirect("accessDenied.jsp");
+//                return;
+//            }
+//            if (employeeManagement.contains(path) && !permisionDao.getPermissionsByUserId(user.getUserId()).contains(permisionDao.getPermissionById(2))) {
+//                res.sendRedirect("accessDenied.jsp");
+//                return;
+//            }
+//        }
+//        //Chuyển tiếp nếu hợp lệ
+        fc.doFilter(request, response);
+
     }
-    
-    public static String getStackTrace(Throwable t) {
-        String stackTrace = null;
-        try {
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            t.printStackTrace(pw);
-            pw.close();
-            sw.close();
-            stackTrace = sw.getBuffer().toString();
-        } catch (Exception ex) {
-        }
-        return stackTrace;
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
-    
-    public void log(String msg) {
-        filterConfig.getServletContext().log(msg);        
-    }
-    
+
 }
